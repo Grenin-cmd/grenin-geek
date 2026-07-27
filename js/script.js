c99ab5d0c0f9bfd2f64d70396f55ef89ba99da46
Grenin <victorblack2005@gmail.com>
2026-07-27 10:15:37 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,6 +23,13 @@ const PRODUCTS = [
   { name: "Blister Triplo Caos Ascendente", category: "pokemon-tcg", price: 42.50, desc: "3 pacotes.", image:"assets/produtos/triple-caos.jpg" },
   { name: "Bleach Remix Volume 2", category: "mangas", price: 45, desc: "Usado." },
   { name: "Bleach Remix Volume 3", category: "mangas", price: 45, desc: "Usado." },
+   { name: "Bleach Remix Volume 4", category: "mangas", price: 45, desc: "Usado." },
+{ name: "Bleach Remix Volume 5", category: "mangas", price: 45, desc: "Usado." },
+{ name: "Radiand Vol.14", category: "mangas", price: 27, desc: "Usado." },
+{ name: "Vagabond Vol. 1", category: "mangas", price: 36.50, desc: "Novo." },
+{ name: "Gantz Vol. 3", category: "mangas", price: 27, desc: "Novo." },
+{ name: "Gantz Vol. 4", category: "mangas", price: 45, desc: "Novo." },
+{ name: "Noragami Vol. 23", category: "mangas", price: 22.99, desc: "Usado." },
   { name: "Sleeves Central (100un)", category: "acessorios", price: 23, desc: "Tamanho padrão para cartas TCG." }
 ];
 
