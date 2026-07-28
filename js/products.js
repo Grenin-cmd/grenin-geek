9635918883642428673147ee0a3dd43a0c311e38
Grenin <victorblack2005@gmail.com>
2026-07-28 10:23:43 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -2,6 +2,8 @@
   const PRODUCTS = [
     { name: "Box Mega Luar Clefable", category: "pokemon-tcg", price: 125, desc: "Caixa fechada, 8 pacotes.", image:"assets/produtos/box-clefable.jpg" },
     { name: "Coleção Arco-Iris Evolções Prismaticas", category: "pokemon-tcg", price: 210, desc: "Caixa fechada, 10 pacotes", image:"assets/produtos/box-eevee.jpg" },
+   { name: "Blister Unitário Equilíbrio Perfeito ", category: "pokemon-tcg", price: 13, desc: "Booster unico", image:"assets/produtos/buniequperf.jpg" },
+{ name: "Blister Unitário Escuridão Absoluta ", category: "pokemon-tcg", price: 13, desc: "Booster unico", image:"assets/produtos/bunicoescabs.jpg" },
     { name: "Blister Triplo Escuridão Absoluta", category: "pokemon-tcg", price: 42.50, desc: "3 pacotes.", image:"assets/produtos/triple-escuridão.jpg" },
     { name: "Blister Triplo Caos Ascendente", category: "pokemon-tcg", price: 42.50, desc: "3 pacotes", image:"assets/produtos/triple-caos.jpg" },
     { name: "Bleach Remix Vol. 2", category: "mangas", price: 45, desc: "Usado", image:"assets/produtos/bleach2.jpg" },
